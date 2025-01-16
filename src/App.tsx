287da73d261479f3e240900956042bab05133bea
Dardan Bujupaj <dardan@ax.tech>
2025-01-16 01:34:38 +0100

    fix tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
           <textarea
             id="content"
             name="content"
-            className="field-sizing-content resize-none block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-teal-600 sm:text-sm/6"
+            className="field-sizing-content resize-none block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-cyan-600 sm:text-sm/6"
           ></textarea>
         </form>
         {output && (
@@ -42,7 +42,7 @@ function App() {
               <QR content={output.content} className="w-full shadow" />
             </div>
 
-            <pre className="bg-teal-100 p-4 rounded-xl">
+            <pre className="bg-cyan-100 p-4 rounded-xl">
               {JSON.stringify(
                 {
                   size: output.code.modules.size,
